711c99c301deb2591eca29e83d6b767cd7ab9dc9
Nguyen The Anh <nguyentheanh17071998@gmail.com>
2021-04-01 22:52:23 +0700

    common of project

---
+++ src/modules/common/components/ProtectedRoute.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
+import { Redirect, Route, RouteProps } from 'react-router';
+import { ROUTES } from '../../../configs/routes';
+
+interface Props extends RouteProps {
+  auth: boolean;
+}
+
+const ProtectedRoute: React.SFC<Props> = (props) => {
+  const { auth, ...restProps } = props;
+  const state = props.location && props.location.state;
+
+  if (auth) {
+    return <Route {...restProps} />;
+  }
+
+  return (
+    <Redirect
+      to={{
+        state,
+        pathname: `${ROUTES.login}`,
+        search: '',
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
