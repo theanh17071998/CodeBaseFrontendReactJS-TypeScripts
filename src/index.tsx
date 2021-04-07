de5ab4c05eb7af9a3345a307336f0bdc6e7b0721
Nguyen The Anh <nguyentheanh17071998@gmail.com>
2021-04-07 23:19:41 +0700

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { MuiThemeProvider } from "@material-ui/core";
+import { ConnectedRouter } from "connected-react-router";
+import { SnackbarProvider } from "notistack";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { MUI_THEME, THEME } from "./configs/setupTheme";
+import "./index.scss";
+import ConnectedIntlProvider from "./modules/intl/components/ConnectedIntlProvider";
+import configureStore, { history } from "./redux/configureStore";
+import * as serviceWorker from "./serviceWorker";
+
+const store = configureStore({});
+const persistor = persistStore(store);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ConnectedIntlProvider>
+          <ThemeProvider theme={THEME}>
+            <MuiThemeProvider theme={MUI_THEME}>
+              <SnackbarProvider maxSnack={3}>
+                <App />
+              </SnackbarProvider>
+            </MuiThemeProvider>
+          </ThemeProvider>
+        </ConnectedIntlProvider>
+      </PersistGate>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
